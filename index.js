3e77bb902f9d065d29549784ec0267b94cba3060
cdnads <93432629+cdnads@users.noreply.github.com>
2021-11-14 13:36:00 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+$("#firstButton").on("click", function() {
+    var inputURL = $("#urlvalue").val();
+    var EncodedURL =encodeURIComponent(inputURL);
+    $("#OutputURL").text(EncodedURL);
+  }); 
+  $("#secondButton").on("click", function() {
+    var inputURL = $("#urlvalue").val();
+    var DecodedURL =decodeURIComponent(inputURL);
+    $("#OutputURL").text(DecodedURL);
+  }); 
+});
